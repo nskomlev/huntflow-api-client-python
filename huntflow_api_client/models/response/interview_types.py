f5f17ab8eac7e609126fd48f7e79ab935f1b2c67
koval <koval@huntflow.ru>
2024-07-22 15:42:33 +0300

    Add get_interview_types() in OrganizationSettings entity.

---
+++ huntflow_api_client/models/response/interview_types.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+from huntflow_api_client.models.consts import InterviewType as InterviewTypeEnum
+
+
+class InterviewType(BaseModel):
+    id: int = Field(..., description="Interview type ID", examples=[20])
+    name: str = Field(..., description="Interview type name", examples=["Phone interview"])
+    account: int = Field(..., description="Organization ID", examples=[42])
+    removed: Optional[datetime] = Field(None, description="Date and time of removing")
+    order: int = Field(..., description="Order number")
+    type: InterviewTypeEnum = Field(
+        ...,
+        description="Type of the interview",
+        examples=[InterviewTypeEnum.USER],
+    )
+
+
+class InterviewTypesListResponse(BaseModel):
+    items: list[InterviewType]
